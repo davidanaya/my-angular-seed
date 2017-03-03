4146623dfe05b5284925d1dd79a002a88791d86a
David Anaya <davidanaya@bcnscience.com>
2017-03-03 09:34:24 +0100

    added webpack for dev and build

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div class="app">
+      Hello David!
+    </div>
+  `
+})
+export class AppComponent {
+}
