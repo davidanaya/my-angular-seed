d1a50f1d526b4a17509d7c926909dc42827eb51d
david <danaya@gmail.com>
2017-03-15 15:20:03 +0100

    added tslint and some issues fixed in webpack conf

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'http://localhost:8080/',
+    //publicPath: 'http://localhost:8080/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
